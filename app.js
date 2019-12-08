ae9ccfa96ab73eb27df5d2b6425f88451245ea40
Evaldas <evaldas.cerniauskas@gmail.com>
2019-12-08 07:59:20 +0200

    Nav bar and animations

--- app.js
+++ app.js
@@ -1,78 +1,25 @@
-let x = 0, bool = false, interval;
-const rotate = () => {
-    const cubes = document.querySelectorAll('.cube');
-  Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
-}
 
-const changePlayPause = () => {
-  const i = document.querySelector('.play-pause i');
-  const cls = i.classList[1];
-  if(cls === 'fa-play') {
-    i.classList.remove('fa-play');
-    i.classList.add('fa-pause');
-  }else {
-    i.classList.remove('fa-pause');
-    i.classList.add('fa-play');
-  }
-  console.log(cls);
-}
 
-const playPause = () => {
-  if(!bool) {
-    interval = setInterval(() => {
-    x -=90;
-    rotate();
-  }, 3000);
-  changePlayPause();
-  bool = true;
-  } else {
-    clearInterval(interval);
-  changePlayPause();
-    bool = false;
-  }
-  
-}
+document.querySelector('.home').addEventListener('click', () => {
+  cubes = document.querySelectorAll('.cube');
 
-document.querySelector('.left-arrow').addEventListener('click', () => {
-  x += 90;
-  rotate();
-  if(bool) {
-    playPause();
-  }
-});
+Array.from(cubes).forEach(cube => cube.style.transform = 'rotateY(90deg)') //transform nodeList to array
+})
 
-document.querySelector('.left-arrow').addEventListener('mouseover', () => {
-  x += 25;
-  rotate();
+document.querySelector('.aboutUs').addEventListener('click', () => {
+   cubes = document.querySelectorAll('.cube');
 
-});
+  Array.from(cubes).forEach(cube => cube.style.transform = 'rotateY(180deg)')
+})
 
-document.querySelector('.left-arrow').addEventListener('mouseout', () => {
-  x -= 25;
-  rotate();
+document.querySelector('.ourTeam').addEventListener('click', () => {
+  cubes = document.querySelectorAll('.cube');
 
-});
+  Array.from(cubes).forEach(cube => cube.style.transform = 'rotateY(270deg)')
+})
 
-document.querySelector('.right-arrow').addEventListener('click', () => {
-  x -= 90;
-  rotate();
-  if (bool) {
-    playPause();
-  }
-});
+document.querySelector('.services').addEventListener('click', () => {
+  cubes = document.querySelectorAll('.cube');
 
-document.querySelector('.right-arrow').addEventListener('mouseover', () => {
-  x -= 25;
-  rotate();
-
-});
-
-document.querySelector('.right-arrow').addEventListener('mouseout', () => {
-  x += 25;
-  rotate();
-
-});
-
-document.querySelector('.play-pause').addEventListener('click', () => {
-  playPause();
+  Array.from(cubes).forEach(cube => cube.style.transform = 'rotateY(360deg)')
 })
